d25aa260066b625fd3a0c363d231aa058c63c0fb
Petr Saprykin <sapryckinpetr4@mail.ru>
2025-11-18 19:04:14 +0300

    feat: add marquee text component

--- services/frontend/src/lib/components/Box.svelte
+++ services/frontend/src/lib/components/Box.svelte
@@ -46,6 +46,7 @@
 <style>
 	.box {
 		width: 100%;
+		min-width: 0;
 		color: var(--color-text-primary);
 		border-radius: var(--radius-default);
 		transition: all 150ms cubic-bezier(0.4, 0, 0.2, 1);
